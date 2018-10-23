de515194f51ee2bc9056fd07f1cd156864b566f5
shuradrozd@mail.ru <bonou2u2>
2018-10-23 19:00:44 +0300

    add new func

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+
+import './todo-list-item.css';
+
+
+export default class TodoListItem extends Component {
+
+   onLabelClick = () => {
+        this.setState((state) => {
+            return {
+                done: !state.done
+            }
+
+        });
+    };
+
+    onMarkImportant = () => {
+        this.setState((state) => {
+            return {
+               important: !state.important
+            };
+        });
+    };
+
+    state = {
+        done: false,
+        important: false
+    };
+
+
+    render() {
+
+        const {label} = this.props;
+        const {done, important} = this.state;
+
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done'
+        }
+        if (important) {
+            classNames += ' important'
+        }
+
+         return (
+          <span className= {classNames}>
+            <span
+            className="todo-list-item-label"
+            onClick={ this.onLabelClick}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right"
+      onClick= { this.onMarkImportant}>
+        <i className="fa fa-exclamation"/>
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o"/>
+      </button>
+    </span>
+            );
+    };
+
+}
--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-const TodoList = () => {
-    const items = ['Learn React', 'Build Awesome App'];
-    return (
-        <ul>
-            <li>{ items[0] }</li>
-            <li>{ items[1] }</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return (
-        <h1>My Todo List</h1>
-    );
-};
-
-const SearchPanel = () => {
-    return (
-        <input type="text" placeholder= 'search'/>
-    );
-};
-
-const App = () => {
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList />
-        </div>
-    )
-};
+import App from './components/app';
 
 ReactDOM.render(<App />,
-    document.getElementById('root'));
+  document.getElementById('root'));
